418c590ff557ebac8fb9a75f2b7962726ae61e1f
devairjr <devair@ymail.com>
2017-06-18 16:49:06 -0300

    Aula 4.3 - Evoluindo nosso cliente Java

--- src/main/java/com/algaworks/socialbooks/aplicacao/Aplicacao.java
+++ src/main/java/com/algaworks/socialbooks/aplicacao/Aplicacao.java
@@ -1,27 +1,37 @@
 package com.algaworks.socialbooks.aplicacao;
 
-import java.net.URI;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
 
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
+import com.algaworks.socialbooks.client.LivrosClient;
+import com.algaworks.socialbooks.client.domain.Livro;
 
 public class Aplicacao {
 
-	public static void main(String[] args){
+	public static void main(String[] args) throws ParseException{
 		
-		RestTemplate restTemplate = new RestTemplate();
+		LivrosClient cliente = new LivrosClient();
 		
-		RequestEntity<Void> request = RequestEntity
-				.get(URI.create("http://localhost:8080/livros"))
-				.header("Authorization", "Basic YWxnYXdvcmtzOnMzbmg0").build();
+		List<Livro> listaLivros = cliente.listar();
 		
-		ResponseEntity<Livro[]> response = restTemplate.exchange(request, Livro[].class);
-		
-		for(Livro livro : response.getBody()){
+		for(Livro livro : listaLivros){
 			System.out.println("Livro: " + livro.getNome());
 		}
 		
+		Livro livro = new Livro();
+		
+		livro.setNome("Git passo-a-passo");
+		livro.setEditora("AlgaWorks");
+		
+		SimpleDateFormat publicacao = new SimpleDateFormat("dd/MM/yyyy");
+		livro.setPublicacao(publicacao.parse("01/01/2017"));
+		
+		livro.setResumo("Este livro aborda tecnicas desenvolvimento de APIs.");
+		
+		String localizacao = cliente.salvar(livro);
+		
+		System.out.println("URI do livro salvo: " + localizacao);
 	}
 	
 }
